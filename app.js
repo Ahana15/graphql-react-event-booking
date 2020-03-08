a32297237d01ad4ad583e710e61822afae4f22a9
Ahana <ahanaghosh94@gmail.com>
2020-03-08 17:20:34 -0400

    add events schema and model

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
-require("dotenv").config();
+
+const Event = require("./models/event");
 const app = express();
 
 const events = [];
@@ -45,19 +46,27 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find().then(events => {
+          return events.map(event => {
+            return { ...event._doc, _id: event.id };
+          });
+        });
       },
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
+          date: new Date(args.eventInput.date)
+        });
 
-        return event;
+        return event
+          .save()
+          .then(res => {
+            console.log(res);
+            return { ...res._doc, _id: event._doc._id.toString() };
+          })
+          .catch(err => console.log(err));
       }
     },
     graphiql: true
@@ -66,7 +75,7 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-pbskw.mongodb.net/test?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-pbskw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(3000);
